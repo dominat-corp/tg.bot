c05d942674142969d5ce33b42f230dd641a0f5b4
Leonid Zharikov <leon.zharikov@gmail.com>
2018-05-11 18:28:55 +0300

    feat: deploy CI #22

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Pulling latest version"
+ssh root@$HOST 'docker pull dominatcorp/tgbot:latest'
+ssh root@$HOST 'docker kill $(docker ps -q)'
+
+echo "Starting..."
+ssh root@$HOST 'docker run -d --restart=always -p 8988:8988 dominatcorp/tgbot:latest'
+
+echo "Success!"
+
+exit 0
